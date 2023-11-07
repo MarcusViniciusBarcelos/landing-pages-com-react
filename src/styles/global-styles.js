c478835b6ec751266bde0cd232f4907640bcd585
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2023-11-07 11:33:52 -0300

    Configurando as fontes

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,10 +1,22 @@
 import { createGlobalStyle, css } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-  }
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 
-  body {}
+  * {
+      margin: 0;
+      padding: 0;
+    }
+
+  html {
+      font-size: 62.5%;
+    }
+
+  body {
+    font-size: 1.6rem;
+    font-family: ${({ theme }) => theme.font.family.default};
+  }
+  h1 {
+    font-family: ${({ theme }) => theme.font.family.secondary};
+  }
 `;
